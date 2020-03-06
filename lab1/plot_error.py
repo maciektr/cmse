31771c754dae11049b36fbfbc0654f43aeeb1350
Maciej <maciek2tr99@gmail.com>
2020-03-06 20:55:23 +0100

    Create makefile

--- lab1/plot_error.py
+++ lab1/plot_error.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt 
 from os import system
-import numpy as np
 
 plot_out = 'plot.txt'
 program = 'z1_steps.o'
 steps = 25000
 
 system('./'+program+' > '+plot_out)
+
 errors = list(map(lambda x: float(x[:-1]),open(plot_out,'r').readlines()))
 x = [i*steps for i in range(len(errors))]
 
@@ -18,3 +18,5 @@ ax.set_xlabel('Number of summations')
 ax.set_ylabel('Value of error')
 
 plt.show()
+
+system('rm '+plot_out)
