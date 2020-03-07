803675a2b27d389f837636565592df1ac0c2e729
Maciej <maciek2tr99@gmail.com>
2020-03-07 18:14:06 +0100

    Plot trajectory

--- lab1/plot_z4.py
+++ lab1/plot_z4.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt 
 from os import system
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', action='store_true')
+args = parser.parse_args()
 
 plot_out = 'plot_z4.txt'
 program = 'z4.o'
-ran = 4. - 1.
+ran = 4. - 1. if not args.n else None
 
 system('./'+program+' > '+plot_out)
 
@@ -11,12 +16,17 @@ values = list(map(lambda x: float(x[:-1]),open(plot_out,'r').readlines()))
 
 fig = plt.figure(figsize=(40,40))
 ax = fig.add_subplot()
-for i in range(10):
-    v = [values[k] for k in range(i,len(values),10)]
-    r = [m*(ran/len(v)) for m in range(len(v))]
-    ax.plot(r,v)
+if not args.n:
+    for i in range(10):
+        v = [values[k] for k in range(i,len(values),10)]
+        r = [m*(ran/len(v)) for m in range(len(v))]
+        ax.plot(r,v)
+    ax.set_xlabel('r')
+else: 
+    r = [i for i in range(len(values))]
+    ax.plot(r,values)
+    ax.set_xlabel('n')
 
-ax.set_xlabel('n')
 ax.set_ylabel('x_n')
 
 plt.savefig('plot_z4.png')
--- lab1/z4.cpp
+++ lab1/z4.cpp
@@ -1,19 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// const float r = 3.7;
-const float x_0 = .7; //0.66666
+const float x_0 = 0.77;
 const int N = 100;
+
+// #define R 1.
 
 int main(){
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    for(float r = 1.; r < 4.; r+=1e-4){
-        float x = x_0;
-        for(int i = 0; i < N+10; i++){
-             if(i>=N)
-                cout<<x<<endl;
-            x = r*x*(1-x);
+    float x = x_0;
+    #ifdef R
+        for(int i = 0; i<N; i++){
+            cout<<x<<endl;
+            x = ((float)R)*(x)*(1.-x);
         }
-    }
+    #else
+        for(float r = 1.; r < 4.; r+=1e-3){
+            x = x_0;
+            for(int i = 0; i < N+10; i++){
+                if(i>=N)
+                    cout<<x<<endl;
+                x = r*x*(1-x);
+            }
+        }
+    #endif
 
 }
