d40ee26677943cade376b1e293ee1f3669225070
Maciej <maciek2tr99@gmail.com>
2020-03-07 22:47:22 +0100

    Render a gif for z4

--- lab1/plot_z4.py
+++ lab1/plot_z4.py
@@ -1,3 +1,4 @@
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt 
 from os import system
 import argparse
@@ -11,24 +12,41 @@ program = 'z4.o'
 ran = 4. - 1. if not args.n else None
 
 system('./'+program+' > '+plot_out)
-
 values = list(map(lambda x: float(x[:-1]),open(plot_out,'r').readlines()))
 
-fig = plt.figure(figsize=(40,40))
-ax = fig.add_subplot()
 if not args.n:
+    fig = plt.figure(figsize=(40,40))
+    ax = fig.add_subplot()
+    ax.set_ylabel('x_n')
     for i in range(10):
         v = [values[k] for k in range(i,len(values),10)]
         r = [m*(ran/len(v)) for m in range(len(v))]
         ax.plot(r,v)
     ax.set_xlabel('r')
+    plt.savefig('plot_z4.png')
 else: 
-    r = [i for i in range(len(values))]
-    ax.plot(r,values)
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.set(ylim=(0., 1.))
+    ax.set_ylabel('x_n')
     ax.set_xlabel('n')
+    r = 1.00
+    ax.set_title('r='+str(r)) 
 
-ax.set_ylabel('x_n')
+    step = 100
+    n = [i for i in range(0,step)]
+    line = ax.plot(n,values[0:step])[0]
 
-plt.savefig('plot_z4.png')
+    def animate(i):
+        line.set_ydata(values[i*step:(i+1)*step])
+        global r
+        r+=1e-2
+        ax.set_title('r='+str(format(round(r,2),'.2f')))
+
+    anim = FuncAnimation(fig, animate, interval=100, frames=len(values)//step)
+    anim.save("plot_z4.gif", writer='imagemagick')
+
+    # plt.draw()
+    # plt.show()
 
 system('rm '+plot_out)
--- lab1/z4.cpp
+++ lab1/z4.cpp
@@ -4,16 +4,23 @@ using namespace std;
 const float x_0 = 0.77;
 const int N = 100;
 
-// #define R 1.
+// #define R 
 
 int main(){
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
     float x = x_0;
     #ifdef R
-        for(int i = 0; i<N; i++){
-            cout<<x<<endl;
-            x = ((float)R)*(x)*(1.-x);
+        for(float r = 1.; r < 4.; r+=1e-2){
+            x = x_0;
+            for(int i = 0; i < N; i++){
+                cout<<x<<endl;
+                x = r*x*(1-x);
+            }
         }
+        // for(int i = 0; i<N; i++){
+        //     cout<<x<<endl;
+        //     x = ((float)R)*(x)*(1.-x);
+        // }
     #else
         for(float r = 1.; r < 4.; r+=1e-3){
             x = x_0;
