4e570663449b47116ef5c36280f9c01adf283484
Maciej <maciek2tr99@gmail.com>
2020-03-22 17:31:12 +0100

    Python implementation of Gauss-Jordan elimination

---
+++ lab2/z1.py
@@ -0,0 +1,28 @@
+import numpy as np 
+
+def scale(x):
+    for i in range(len(x)):
+        x[i] = x[i] / max(x[i])
+    return x 
+
+if __name__ == "__main__":
+    n = 500
+    A = np.random.uniform(-1*10**6,10**6,size=(n,n))
+    B = np.random.uniform(-1*10**6,10**6,size=(n,1))
+    print("Numpy solution:\n",np.linalg.solve(A,B).reshape((n)))
+
+    AB = scale(np.hstack((A,B)))
+
+    for i in range(n):
+        for k in range(n):
+            if i==k:
+                continue
+            AB[k] = AB[k] * (AB[i][i] / AB[k][i])
+            AB[k] -= AB[i]
+            AB[k][i] = 0.
+
+    res = []
+    for i in range(n):
+        res.append(AB[i][n] / AB[i][i])
+
+    print("My elimination solution:\n", np.array(res))
