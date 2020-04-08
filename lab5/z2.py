5b1c749ccd4bdb34f48e0e3a9b08c4dbcb308939
maciektr <mtratnowiecki@gmail.com>
2020-04-08 15:20:24 +0200

    Working compression

--- lab5/z2.py
+++ lab5/z2.py
@@ -1,29 +1,25 @@
 import numpy as np
 # Since scipy.imread is deprecated
 import imageio
-
+from skimage import img_as_ubyte
 
 def low_rank_approx(img, k):
-    res = [None for _ in range(img.shape[2])]
+    res = np.empty(shape=img.shape)
     for m in range(img.shape[2]):
         u, s, vh = np.linalg.svd(img[:, :, m])
-        comp = (u @ vh) * s[0]
-        for i in range(1, k):
-            comp += (u @ vh) * s[i]
-        res[m] = comp
-
-    res = np.array(res)
-    res = np.moveaxis(res, 0, -1)
-
-    print(img[0][0])
-    print(res[0][0])
-    print(res.shape)
+        comp = np.zeros(shape=img.shape[:2])
+        for i in range(k):
+            x = np.zeros(shape=img.shape[:2])
+            x[i][i] = s[i]
+            comp += u @ x @ vh
+        res[:, :, m] = comp
     return res
 
 
 if __name__ == '__main__':
     path = 'Lenna.png'
-    comp_path = 'LennaCompressed.jpg'
+    comp_path = 'LennaCompressed.png'
+    rank = 60
+
     img = imageio.imread(path)
-    imageio.imsave(comp_path, low_rank_approx(img, 512))
-    # low_rank_approx(img, 100)
+    imageio.imsave(comp_path, low_rank_approx(img, rank))
