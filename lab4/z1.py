591d0d4d30ff6e60d426a0eb1297aed4946ed91d
maciektr <mtratnowiecki@gmail.com>
2020-04-10 00:02:29 +0200

    Reformat

--- lab4/z1.py
+++ lab4/z1.py
@@ -20,19 +20,29 @@ def dist(a, b):
 def loss(points):
     res = 0
     n = len(points)
-    for i in range(n + 1):
+    for i in range(n):
         res += (dist(points[i % n], points[(i + 1) % n]))
     return res
 
 
 def consecutive_swap(points):
+    points = list(points)
     x = randint(0, len(points) - 1)
     y = (x + 1) % len(points)
     points[x], points[y] = points[y], points[x]
     return points
 
 
+def arbitrary_swap(points):
+    points = list(points)
+    x = randint(0, len(points) - 1)
+    y = randint(0, len(points) - 1)
+    points[x], points[y] = points[y], points[x]
+    return points
+
+
 def reverse_swap(points):
+    points = list(points)
     x = randint(0, len(points) - 1)
     y = randint(0, len(points) - 1)
     if y < x:
@@ -41,14 +51,8 @@ def reverse_swap(points):
     return points
 
 
-def arbitrary_swap(points):
-    x = randint(0, len(points) - 1)
-    y = randint(0, len(points) - 1)
-    points[x], points[y] = points[y], points[x]
-    return points
-
-
 def greedy_init(points):
+    points = list(points)
     for i in range(1, len(points) - 1):
         for k in range(i + 1, len(points)):
             if dist(points[i - 1], points[k]) < dist(points[i - 1], points[i]):
@@ -56,30 +60,34 @@ def greedy_init(points):
     return points
 
 
-def sannealing(points, choose, steps, temp, alpha, history=None):
-    best = {'points': [], 'loss': math.inf}
-    n = steps
-    t0 = temp
-    while steps > 0 and temp > 1e-8:
-        neigh = choose(list(points))
-        n_los = loss(neigh)
-        d = loss(points) - n_los
-        if n_los < best['loss']:
-            best['loss'] = n_los
-            best['points'] = neigh.copy()
-        if d > 0 or random.random() <= math.exp(-abs(d) / temp):
-            points = neigh
+def hist_update(history, id, neigh_loss, neigh, all):
+    if history is not None and history['all'] == all:
+        history['list'].append({'id': id,
+                                'loss': round(neigh_loss, 2),
+                                'points': None if not history['copy'] else list(neigh) + [neigh[0]]
+                                })
 
-            if history is not None and history['all'] is False:
-                history['list'].append({'id': n - steps,
-                                        'loss': round(loss(points), 2),
-                                        'points': None if not history['copy'] else list(points) + [points[0]]
-                                        })
-        if history is not None and history['all'] is True:
-            history['list'].append({'id': n - steps,
-                                    'loss': round(loss(neigh), 2),
-                                    'points': None if not history['copy'] else list(neigh) + [neigh[0]]
-                                    })
+
+def sannealing(points, choose, steps, temp, alpha, history=None):
+    points_loss = loss(points)
+    best = {'points': points, 'loss': points_loss}
+    steps_0 = steps
+    temp_0 = temp
+
+    while steps > 0 and temp > 1e-8:
+        neigh = choose(points)
+        neigh_loss = loss(neigh)
+        if  best['loss'] > neigh_loss:
+            best['loss'] = neigh_loss
+            best['points'] = list(neigh)
+
+        diff = points_loss - neigh_loss
+        if diff > 0 or random.random() <= math.exp(-abs(diff) / temp):
+            points = list(neigh)
+            points_loss = neigh_loss
+
+            hist_update(history, steps_0 - steps, neigh_loss, neigh, False)
+        hist_update(history, steps_0 - steps, neigh_loss, neigh, True)
         steps -= 1
         temp *= alpha
     return best['points']
@@ -87,8 +95,7 @@ def sannealing(points, choose, steps, temp, alpha, history=None):
 
 def get_points(n=20, v=1000, type='uniform'):
     if type == 'normal':
-        # return np.random.normal((0, 0), (v, v), (n, 2))
-        return [[random.gauss(0,v),random.gauss(0,v)] for _ in range(n)]
+        return [[random.gauss(0, v), random.gauss(0, v)] for _ in range(n)]
     if type == 'groups':
         k = v // 3
         res = []
@@ -96,8 +103,6 @@ def get_points(n=20, v=1000, type='uniform'):
         for _ in range(3):
             x = -2 * k
             for _ in range(3):
-                # r = np.random.uniform(-k, k, (n // 9, 2)) + np.array([x, y])
-                # res = np.concatenate((res, r))
                 r = [[random.uniform(-k, k) + x, random.uniform(-k, k) + y] for _ in range(n // 2)]
                 res = res + r
                 x += 2 * k
@@ -107,19 +112,20 @@ def get_points(n=20, v=1000, type='uniform'):
 
 
 if __name__ == '__main__':
-    n = 500
+    n = 100
     v = 10000
-    points = get_points(n, v, 'normal')
-    # points = [[random.uniform(-1000, 1000), random.uniform(-1000, 1000)] for i in range(100)]
+    points = get_points(n, v, 'uniform')
+    # draw_solution(points)
 
-    draw_solution(points)
-    hist = {'list': [], 'copy': False, 'all': True}
-    # hist = None
     points = greedy_init(points)
-    los = loss(points)
-    draw_solution(points)
-    points = sannealing(points, consecutive_swap, 100000, 22, 0.995, hist)
-    draw_solution(points)
+    # draw_solution(points)
+
+    init_loss = loss(points)
+    hist = {'list': [], 'copy': False, 'all': False}
+
+    points = sannealing(points, consecutive_swap, 100000, math.sqrt(len(points)), 0.995, hist)
+    # draw_solution(points)
+
     # plot_history.plot_anim(hist)
     plot_history.plot_loss(hist)
-    print("Obtained ", str(round(100 * (los - loss(points)) / los, 2)), "% improvement over greedy.")
+    print("Obtained ", str(round(100 * (init_loss - loss(points)) / init_loss, 2)), "% improvement over initial state.")
