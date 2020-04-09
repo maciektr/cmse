a31f58ed1f43aeafb1e9ebd4d11fd84866a5ae3d
maciektr <mtratnowiecki@gmail.com>
2020-04-09 13:53:35 +0200

    History as dict

---
+++ lab4/plot_history.py
@@ -0,0 +1,29 @@
+from matplotlib.animation import FuncAnimation
+from matplotlib import pyplot as plt
+
+
+def plot_anim(history, out_path='history.gif'):
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    # ax.set(ylim=(0., 1.))
+    ax.set_ylabel('y')
+    ax.set_xlabel('x')
+
+    ax.set_title('step=' + str(history['list'][0]['id']) + ' loss=' + str(history['list'][0]['loss']))
+    line = ax.plot(*history['list'][0]['points'].T, marker='o')[0]
+
+    def animate(i):
+        line.set_data(*history['list'][i]['points'].T)
+        ax.set_title('step=' + str(history['list'][i]['id']) + ' loss=' + str(history['list'][i]['loss']))
+
+    anim = FuncAnimation(fig, animate, interval=100, frames=len(history['list']))
+    anim.save(out_path, writer='imagemagick')
+
+
+def plot_loss(history):
+    fig = plt.figure("Loss change", figsize=(10, 10))
+    ax = plt.axes()
+    steps = list(map(lambda x: x.get('id'), history['list']))
+    loss = list(map(lambda x: x.get('loss'), history['list']))
+    ax.plot(steps, loss)
+    plt.show()
--- lab4/z1.py
+++ lab4/z1.py
@@ -4,7 +4,7 @@ import plot_history
 import numpy as np
 
 
-def draw_solution(points: np.array):
+def draw_solution(points):
     fig = plt.figure("TSP", figsize=(10, 10))
     ax = plt.axes()
     p = np.concatenate((points, np.array([points[0]])))
@@ -16,7 +16,7 @@ def dist(x, y):
     return np.sqrt(np.sum((x - y) ** 2))
 
 
-def loss(points: np.array):
+def loss(points):
     res = 0
     n = len(points)
     for i in range(n + 1):
@@ -24,51 +24,83 @@ def loss(points: np.array):
     return res
 
 
-def consecutive_swap(points: np.array):
+def consecutive_swap(points):
     x = randint(0, len(points) - 1)
     y = (x + 1) % len(points)
     points[[x, y]] = points[[y, x]]
     return points
 
 
-def flip_swap(points: np.array):
+def flip_swap(points):
     x = randint(0, len(points) - 1)
     y = randint(0, len(points) - 1)
     points[x:y] = np.flip(points[x:y])
     return points
 
 
-def arbitrary_swap(points: np.array):
+def arbitrary_swap(points):
     x = randint(0, len(points) - 1)
     y = randint(0, len(points) - 1)
     points[[x, y]] = points[[y, x]]
     return points
 
 
-def sannealing(points: np.array, choose, steps, temp, alpha, history=None, copy=False):
+# def greedy_init(points):
+
+
+def sannealing(points, choose, steps, temp, alpha, history=None):
     n = steps
     while steps > 0 and temp > 0:
         neigh = choose(points.copy())
         d = loss(points) - loss(neigh)
         if d > 0 or np.random.rand(1) <= np.exp(d / temp):
             points = neigh
-            if history is not None:
-                history.append({'id': n - steps,
-                                'loss': round(loss(points), 2),
-                                'points': None if not copy else np.concatenate((points, np.array([points[0]])))
-                                })
+            if history is not None and history['all'] == False:
+                history['list'].append({'id': n - steps,
+                                        'loss': round(loss(points), 2),
+                                        'points': None if not history['copy'] else np.concatenate(
+                                            (points, np.array([points[0]])))
+                                        })
+        if history is not None and history['all'] == True:
+            history['list'].append({'id': n - steps,
+                                    'loss': round(loss(neigh), 2),
+                                    'points': None if not history['copy'] else np.concatenate(
+                                        (neigh, np.array([neigh[0]])))
+                                    })
         steps -= 1
         temp *= alpha
     return points
 
 
+def get_points(n=20, v=1000, type='uniform'):
+    if type == 'normal':
+        return np.random.normal((0, 0), (v, v), (n, 2))
+    if type == 'groups':
+        k = v // 3
+        res = np.empty(shape=(0, 2))
+        y = -2 * k
+        for _ in range(3):
+            x = -2 * k
+            for _ in range(3):
+                r = np.random.uniform(-k, k, (n // 9, 2)) + np.array([x, y])
+                res = np.concatenate((res, r))
+                x += 2 * k
+            y += 2 * k
+        return res
+
+    return np.random.uniform(-v, v, (n, 2))
+
+
 if __name__ == '__main__':
-    n = 1000
-    v = 100000
-    points = np.random.uniform(-v, v, (n, 2))
+    n = 500
+    v = 10000
+    points = get_points(n, v, 'groups')
     draw_solution(points)
-    hist = []
-    points = sannealing(points, arbitrary_swap, 1000, 100, 0.9994, hist)
+    hist = {'list': [], 'copy': False, 'all': True}
+    # hist = None
+    # print(loss(points))
+    points = sannealing(points, consecutive_swap, 4000, 200, 0.9994, hist)
+    # print(loss(points))
     draw_solution(points)
     # plot_history.plot_anim(hist)
     plot_history.plot_loss(hist)
