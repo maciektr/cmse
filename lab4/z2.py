43be8a336662ea2c903f3b1532d468f049a815a8
maciektr <mtratnowiecki@gmail.com>
2020-04-14 22:51:01 +0200

    Bug fix - not running adjacency filter in each case

--- lab4/z2.py
+++ lab4/z2.py
@@ -14,22 +14,23 @@ def random_binary(n, density):
     return img.tolist()
 
 
-def adjacent_filter(ind, n=None):
+def adjacent_filter(ind, n):
     if n is not None:
         ind = filter(lambda k: (k[0] < n and k[1] < n), ind)
-    return list(filter(lambda k: (k[0] >= 0 and k[1] >= 0), ind))
+    res = list(filter(lambda k: (k[0] >= 0 and k[1] >= 0), ind))
+    return res
 
 
-def adjacent_four(x, y, n=None):
+def adjacent_four(x, y, n):
     return adjacent_filter(map(lambda k: (k[0] + x, k[1] + y), zip((-1, 0, 1, 0), (0, -1, 0, 1))), n)
 
 
-def adjacent_eight(x, y, n=None):
-    return adjacent_four(x, y) + adjacent_filter(
+def adjacent_eight(x, y, n):
+    return adjacent_four(x, y, n) + adjacent_filter(
         map(lambda k: (k[0] + x, k[1] + y), zip((-1, 1, 1, -1), (-1, -1, 1, 1))), n)
 
 
-def adjacent_sixteen(x, y, n=None):
+def adjacent_sixteen(x, y, n):
     res = []
     for i in range(-2, 2 + 1):
         res.append((-2 + x, i + y))
@@ -40,17 +41,26 @@ def adjacent_sixteen(x, y, n=None):
     return adjacent_filter(res, n)
 
 
-def adjacent_sixteen_eight(x, y):
-    return adjacent_eight(x, y) + adjacent_sixteen(x, y)
+def adjacent_sixteen_eight(x, y, n):
+    return adjacent_eight(x, y, n) + adjacent_sixteen(x, y, n)
 
 
-def cost_function(img, adjacency, energy):
+def cost_function(img, adjacency, energy, changed=None, prev_img=None, prev_cost=None):
     res = 0
     n = len(img)
-    for x in range(n - 1):
-        for y in range(n - 1):
-            for ind in adjacency(x, y, n):
-                res += energy(img, x, y, ind[0], ind[1])
+    if changed is None:
+        for x in range(n - 1):
+            for y in range(n - 1):
+                for ind in adjacency(x, y, n):
+                    res += energy(img, x, y, ind[0], ind[1])
+    else:
+        res = prev_cost
+        for pos in changed:
+            adj = adjacency(pos[0], pos[1], 20)
+            for ind in adj:
+                res -= energy(prev_img, pos[0], pos[1], ind[0], ind[1])
+                res += energy(img, pos[0], pos[1], ind[0], ind[1])
+
     return res
 
 
@@ -60,8 +70,8 @@ def dist(x, y, k, m):
 
 def energy_simple(img, x, y, k, m):
     d = dist(x, y, k, m)
-    if img[x][y] == img[k][m]:
-        return 5 / d
+    if img[x][y] == img[k][m] == 1:
+        return -5 / d
     return 0
 
 
@@ -72,17 +82,24 @@ def choose_img(img):
     x, y = randint(0, n - 1), randint(0, n - 1)
     k, m = randint(0, n - 1), randint(0, n - 1)
     img[x][y], img[k][m] = img[k][m], img[x][y]
-    return img
+    return img, [(x, y), (k, m)]
 
 
-def annealing(img, adjacency, energy, steps, temp, alpha):
+def hist_update(img, cost, history):
+    if history is not None:
+        history['list'].append({'id': id,
+                                'loss': round(neigh_loss, 2),
+                                'points': None if not history['copy'] else list(neigh) + [neigh[0]]
+                                })
+
+
+def annealing(img, adjacency, energy, steps, temp, alpha, hist=None):
     cost = cost_function(img, adjacency, energy)
     best = {'img': list(img), 'cost': cost}
 
     while steps > 0 and temp > 1e-8:
-        print('step')
-        new_img = choose_img(img)
-        new_cost = cost_function(new_img, adjacency, energy)
+        new_img, changed = choose_img(img)
+        new_cost = cost_function(new_img, adjacency, energy, changed, img, cost)
         if best['cost'] > new_cost:
             best['cost'] = new_cost
             best['img'] = list(new_img)
@@ -92,7 +109,7 @@ def annealing(img, adjacency, energy, steps, temp, alpha):
             img = new_img
             cost = new_cost
 
-        #     hist_update(history, steps_0 - steps, neigh_loss, neigh, False)
+            # hist_update(history, steps_0 - steps, neigh_loss, neigh, False)
         # hist_update(history, steps_0 - steps, neigh_loss, neigh, True)
         steps -= 1
         temp *= alpha
@@ -104,7 +121,17 @@ if __name__ == '__main__':
     before_path = 'img1.png'
     after_path = 'img2.png'
     # density = [0.1, 0.3, 0.4]
-    img = random_binary(256, 0.1)
+
+    n = 32
+    density = 0.4
+    steps = 2500
+    temp = 80
+    alpha = 0.9995
+
+    img = random_binary(n, density)
     imageio.imsave(before_path, img)
-    annealing(img, adjacent_eight, energy_simple, 50, math.sqrt(256), 0.995)
+
+    hist = {'list':[]}
+    annealing(img, adjacent_eight, energy_simple, steps, temp, alpha, hist)
+
     imageio.imsave(after_path, img)
