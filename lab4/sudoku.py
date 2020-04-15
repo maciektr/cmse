4eb837ff5c9ce25ed19197b91db7bac598e66db6
maciektr <mtratnowiecki@gmail.com>
2020-04-15 22:06:56 +0200

    Cost function in sudoku class

--- lab4/sudoku.py
+++ lab4/sudoku.py
@@ -2,6 +2,7 @@ class Sudoku:
     def __init__(self):
         self.size = 9
         self.board = [[None for _ in range(self.size)] for _ in range(self.size)]
+        self.cost_value = None
 
     def read_from_file(self, path):
         with open(path, 'r') as f:
@@ -19,6 +20,10 @@ class Sudoku:
     def block_start(block_id):
         return block_id // 3 * 3, block_id % 3 * 3
 
+    @staticmethod
+    def block_id(point):
+        return point[0] // 3 * 3 + point[1] // 3
+
     def get_from_block(self, block_id):
         x, y = self.block_start(block_id)
         res = set()
@@ -31,20 +36,55 @@ class Sudoku:
                 res.add(el)
         return res, conflicts
 
-    def cost(self):
+    def col_row_cost(self, x, y):
         res = 0
-        seen_col = [set() for _ in range(self.size)]
-        for i in range(self.size):
-            seen_row = set()
-            for k in range(self.size):
-                el = self.board[i][k]
-                if el in seen_col[k]:
-                    res += 1
-                seen_col[k].add(el)
-                if el in seen_row:
-                    res += 1
-                seen_row.add(el)
-        for i in range(0, 8 + 1):
-            el, con = self.get_from_block(i)
-            res += con
+        seen_col = set()
+        seen_row = set()
+        for i in range(0, self.size):
+            col_el = self.board[x][i]
+            row_el = self.board[i][y]
+            if col_el in seen_col:
+                res += 1
+            seen_col.add(col_el)
+            for row_el in seen_row:
+                res += 1
+            seen_row.add(row_el)
         return res
+
+    def cost(self, changed=None, previous=None):
+        if self.cost_value is not None:
+            return self.cost_value
+        res = 0
+        if changed is None:
+            seen_col = [set() for _ in range(self.size)]
+            for i in range(self.size):
+                seen_row = set()
+                for k in range(self.size):
+                    el = self.board[i][k]
+                    if el in seen_col[k]:
+                        res += 1
+                    seen_col[k].add(el)
+                    if el in seen_row:
+                        res += 1
+                    seen_row.add(el)
+            for i in range(0, 8 + 1):
+                el, con = self.get_from_block(i)
+                res += con
+        else:
+            res = previous.cost()
+            x, y = changed
+            res -= previous.col_row_cost(x, y)
+            res += self.col_row_cost(x, y)
+            bid = Sudoku.block_id(changed)
+            res -= previous.get_from_block(bid)[1]
+            res += self.get_from_block(bid)[1]
+        self.cost_value = res
+        return res
+
+    def copy(self):
+        res = Sudoku()
+        for i in range(0, self.size):
+            for k in range(0, self.size):
+                res.board[i][k] = self.board[i][k]
+        res.cost_value = self.cost_value
+        return res  
