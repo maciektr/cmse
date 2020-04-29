31a90d60e0f9dfe07a94962d9a3605f9b6fe70c2
maciektr <mtratnowiecki@gmail.com>
2020-04-29 16:17:27 +0200

    Rename function

--- lab7/rayleigh.py
+++ lab7/rayleigh.py
@@ -4,7 +4,7 @@ import scipy.linalg
 from power_method import power_method
 
 
-def rayleigh_iteration(matrix, sigma, eps=10 ** -6, steps=10 ** 5):
+def rayleigh_method(matrix, sigma, eps=10 ** -6, steps=10 ** 5):
     vector = np.random.rand(matrix.shape[0])
     k = 0
     while k < steps:
@@ -21,7 +21,7 @@ def rayleigh_iteration(matrix, sigma, eps=10 ** -6, steps=10 ** 5):
 if __name__ == '__main__':
     matrix = rand_sym_matrix(4)
     _, sigma = power_method(matrix, steps=10)
-    vector, dominant = rayleigh_iteration(matrix, sigma)
+    vector, dominant = rayleigh_method(matrix, sigma)
 
     print(vector)
     print(dominant)
