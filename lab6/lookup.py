c6292902d3f6e31863a6aadf21b2ebc2ec31f6c0
maciektr <mtratnowiecki@gmail.com>
2020-05-27 18:59:55 +0200

    Add multithreaded get_all_words function

--- lab6/lookup.py
+++ lab6/lookup.py
@@ -1,5 +1,6 @@
 from spacy.tokenizer import Tokenizer
 from spacy.lang.en import English
+import multiprocessing
 import os
 
 
@@ -11,16 +12,17 @@ def tokenize_file(path):
     return list(map(str, tokens))
 
 
-def get_all_words(folder_path, verbose=False):
-    res = []
+def get_all_words(folder_path):
     path = os.path.abspath(folder_path)
-    for filename in os.listdir(path):
-        if verbose:
-            print("Tokenizing file", filename)
-        res += tokenize_file(os.path.join(path, filename))
-    return res
+    with multiprocessing.Pool(processes=8) as pool:
+        res = pool.map(tokenize_file, [os.path.join(path, filename) for filename in os.listdir(path)])
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    return flatten(res)
 
 
 if __name__ == '__main__':
     folder = 'wiki'
-    print(get_all_words(folder))
+    # folder = 'test_dir'
+    res = get_all_words(folder)
+    # print(res)
+    print(len(res))
