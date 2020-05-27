baca66fd169be30a29e50c5d6cffa45679ba3e7b
maciektr <mtratnowiecki@gmail.com>
2020-05-27 21:50:26 +0200

    Use html2text, use sets instead of lists

--- lab6/lookup.py
+++ lab6/lookup.py
@@ -1,28 +1,31 @@
-from spacy.tokenizer import Tokenizer
-from spacy.lang.en import English
 import multiprocessing
+import html2text
 import os
 
 
 def tokenize_file(path):
-    tokenizer = Tokenizer(English().vocab)
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    h.ignore_images = True
+    h.ignore_tables = True
+    h.ignore_emphasis = True
     with open(path, 'r') as file:
-        text = ''.join(file.readlines())
-        tokens = tokenizer(text)
-    return list(map(str, tokens))
+        return set(h.handle(file.read()).split())
 
 
 def get_all_words(folder_path):
     path = os.path.abspath(folder_path)
     with multiprocessing.Pool(processes=8) as pool:
         res = pool.map(tokenize_file, [os.path.join(path, filename) for filename in os.listdir(path)])
-    flatten = lambda l: [item for sublist in l for item in sublist]
-    return flatten(res)
+    result = set()
+    for r in res:
+        result = result | r
+    return list(result)
 
 
 if __name__ == '__main__':
-    folder = 'wiki'
-    # folder = 'test_dir'
+    # folder = 'wiki'
+    folder = 'test_dir'
     res = get_all_words(folder)
-    # print(res)
+    print(res)
     print(len(res))
