fb70a96be0cc99119d5821f988f8287e559ac479
maciektr <mtratnowiecki@gmail.com>
2020-06-10 14:00:43 +0200

    Start zad2 implemnetation

---
+++ lab9/ocr.py
@@ -0,0 +1,11 @@
+from numpy import fft
+
+
+class Ocr:
+    def __init__(self):
+        self.chars_order = [chr(i) for i in range(ord('a'), ord('z'))]\
+            + [chr(i) for i in range(ord('A'), ord('Z')]
+            + 
+
+    def load_alphabet(self, alphabet_path):
+        pass
