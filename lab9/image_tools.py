2c61090fad094ee4adca5e25f155d5cb4d00f36f
maciektr <mtratnowiecki@gmail.com>
2020-06-13 12:42:20 +0200

    Use invert argument

--- lab9/image_tools.py
+++ lab9/image_tools.py
@@ -7,21 +7,16 @@ def convert_img(img, invert=True):
     if invert:
         img = PIL.ImageOps.invert(img)
     img = np.asarray(img)
-    # gray_img = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
-    # if invert:
-    #     gray_img = np.ones(gray_img.shape) * .255 - gray_img
-    # normalised = gray_img * 255 / np.max(gray_img)
-    # return normalised
     return img
 
 
 def read_img(path, invert=True):
-    img = Image.open(path)
-    # return convert_img(img, invert)
-    read = np.asarray(PIL.ImageOps.invert(img.convert('L')))
+    img = Image.open(path).convert('L')
+    if invert:
+        img = PIL.ImageOps.invert(img)
+    read = np.asarray(img)
     return read
 
 
 if __name__ == '__main__':
-    # img = Image.open('alphabet.png').convert('L').save('b.png')
     pass
